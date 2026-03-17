a1263478c09705c0c1acd31deeef6b3a53ccd051
Pawiel Hrusha <s32564@pjwstk.edu.pl>
2026-03-17 20:29:07 +0100

    Second num and summing code added

--- App/App/Program.cs
+++ App/App/Program.cs
@@ -5,3 +5,7 @@ if (number % 2 == 0)
     Console.WriteLine("The number is even");
 else
     Console.WriteLine("The number is odd");
+
+Console.WriteLine("Enter another number: ");
+int number2 = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+Console.WriteLine($"The sum of entered numbers is {number +  number2}");
