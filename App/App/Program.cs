c3840246d02b2969204be52de35cdd25083cae6f
Pawiel Hrusha <s32564@pjwstk.edu.pl>
2026-03-17 20:33:29 +0100

    Letter counter added

--- App/App/Program.cs
+++ App/App/Program.cs
@@ -4,3 +4,12 @@ int number = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException
 Console.WriteLine("Enter another number: ");
 int number2 = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
 Console.WriteLine($"The sum of entered numbers is {number +  number2}");
+
+Console.WriteLine("Enter any word: ");
+string? word = Console.ReadLine();
+int counter = 0;
+foreach (char c in word)
+{
+    counter++;
+}
+Console.WriteLine($"The number of letters in the entered word is {counter}");
