3908552256a4952d948a02ccc8d7bceb30655f74
Pawiel Hrusha <s32564@pjwstk.edu.pl>
2026-03-17 20:55:19 +0100

    Added CalculateAverage function

--- App/App/Program.cs
+++ App/App/Program.cs
@@ -1,15 +1,43 @@
-﻿Console.WriteLine("Enter number: ");
-int number = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+﻿namespace App;
 
-Console.WriteLine("Enter another number: ");
-int number2 = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
-Console.WriteLine($"The sum of entered numbers is {number +  number2}");
-
-Console.WriteLine("Enter any word: ");
-string? word = Console.ReadLine();
-int counter = 0;
-foreach (char c in word)
+public class MainClass
 {
-    counter++;
+    public static void Main(String[] args)
+    {
+        Console.WriteLine("Enter number: ");
+        int number = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+
+        Console.WriteLine("Enter another number: ");
+        int number2 = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
+        Console.WriteLine($"The sum of entered numbers is {number + number2}");
+
+        Console.WriteLine("Enter any word: ");
+        string word = Console.ReadLine();
+        if (word is not null)
+        {
+            int counter = 0;
+            foreach (char c in word)
+            {
+                counter++;
+            }
+
+            Console.WriteLine($"The number of letters in the entered word is {counter}");
+        }
+
+        int[] values = { number, number2 };
+        Console.WriteLine($"The average number is: {CalculateAverage(values)}");
+    }
+
+    private static double CalculateAverage(int[] values)
+    {
+        int counter = 0;
+        int sum = 0;
+        foreach (int num in values)
+        {
+            counter++;
+            sum += num;
+        }
+
+        return (double) sum / counter;
+    }
 }
-Console.WriteLine($"The number of letters in the entered word is {counter}");
