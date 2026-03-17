22d462b678e6896b74335619b85d4e74e95114ed
Pawiel Hrusha <s32564@pjwstk.edu.pl>
2026-03-17 20:04:49 +0100

    Initial project

---
+++ App/App/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Enter number: ");
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 == 0)
+    Console.WriteLine("The number is even");
+else
+    Console.WriteLine("The number is odd");
