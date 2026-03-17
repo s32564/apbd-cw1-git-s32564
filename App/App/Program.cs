d8ba30e14ba05ebaa828b7af1faa0bcbd85d098e
Pawiel Hrusha <s32564@pjwstk.edu.pl>
2026-03-17 20:23:44 +0100

    Error handling added

--- App/App/Program.cs
+++ App/App/Program.cs
@@ -1,5 +1,5 @@
 ﻿Console.WriteLine("Enter number: ");
-int number = int.Parse(Console.ReadLine());
+int number = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
 
 if (number % 2 == 0)
     Console.WriteLine("The number is even");
