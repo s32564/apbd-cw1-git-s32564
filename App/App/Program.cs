8f8fee3b414a8bcbc408c10678ee7b21dddb9684
Pawiel Hrusha <s32564@pjwstk.edu.pl>
2026-03-17 20:30:15 +0100

    even/odd checker deleted

--- App/App/Program.cs
+++ App/App/Program.cs
@@ -1,11 +1,6 @@
 ﻿Console.WriteLine("Enter number: ");
 int number = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
 
-if (number % 2 == 0)
-    Console.WriteLine("The number is even");
-else
-    Console.WriteLine("The number is odd");
-
 Console.WriteLine("Enter another number: ");
 int number2 = int.Parse(Console.ReadLine() ?? throw new InvalidOperationException());
 Console.WriteLine($"The sum of entered numbers is {number +  number2}");
